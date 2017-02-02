aa97e86d445e62e17727c825c8bb937820c2e65a
Jason Clegg <jgcdeveloper@gmail.com>
2017-02-01 21:10:40 -0700

    Update app to generate seed data for Users and Wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create 10 Random Users. User will be created with default role
+
+puts "Seeding Users"
+
+10.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "123456",
+    confirmed_at: Date.today
+  )
+end
+
+# Collect all the randomly seeded users
+users = User.all
+
+puts "Seeding Wikis"
+
+# Create 20 Random Wikis.
+20.times do
+  Wiki.create!(
+    title: Faker::Lorem.sentence(6),
+    body: Faker::Lorem.paragraph(4),
+    user_id: users.sample.id,
+    private: false
+  )
+end
+
+puts "Seeding Finished!"
+puts "#{User.count} users seeded (including 1 admin)"
+puts "#{Wiki.count} wikis seeded"
