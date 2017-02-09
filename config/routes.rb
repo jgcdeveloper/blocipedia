0e42a3e7a1ce7faf282ad627c8962c10ad6202ff
Jason Clegg <jgcdeveloper@gmail.com>
2017-02-09 16:46:58 -0700

    Finish Adding Upgrade/Downgrade Setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { :registrations => 'users/registrations'}
+
+  devise_scope :user do
+    post "users/downgrade", :to => 'users/registrations#downgrade'
+  end
 
   resources :wikis
 
